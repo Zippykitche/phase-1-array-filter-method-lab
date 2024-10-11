2b7b3dc56e0ebbc559cd6f3dc1cb069a222ec135
zippykitche <zippyk80@gmail.com>
2024-10-11 11:22:59 +0300

    filter lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(drivers, names){
+    return drivers.filter(driver => driver.toLowerCase() === names.toLowerCase());
+}
+
+function fuzzyMatch(drivers, letters){
+    return drivers.filter(driver => driver.toLowerCase().startsWith(letters.toLowerCase()));
+}
+
+function matchName (drivers, name){
+    return drivers.filter(driver => driver.name === name);
+}
